3ca556ba29e303965cadaf145a494d2b32d35b55
Dr-bijay-neupane <bijayneupane433@gmail.com>
2025-12-08 18:29:01 +0545

    ADDING CODE FILE

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 print("hello world")
+
+
+
+name=input("Enter your name")
+print(name)
+
+for i in range(10):
+    print(i)
+
+def sum(a,b):
+    sum=a+b
+    return sum
+x=int(input("Enter numbers"))
+n=sum(x,3)
+print("the sum of x and y is=",n)
+
+
+i=0
+while(i<=10):
+    print(i)
+    i+=1
