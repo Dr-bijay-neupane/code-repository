ec594fd86ce9956d7c72b801b3ac199b6ac1537c
Dr-bijay-neupane <bijayneupane433@gmail.com>
2025-12-09 17:14:45 +0545

    FUNCTIONS

---
+++ coding.py
@@ -0,0 +1,20 @@
+
+def calculator(choice):
+    a=int(input("ENTER ANY VALUE"))
+    b=int(input("ENTER ANY VALUE"))
+    match choice:
+        case 1:
+            return a+b
+
+        case _:
+            print("INVALID")
+            
+    
+
+
+c=int(input("Enter value l. ADDING  2.SUBTRACTION 3.MULTIPLICATIN 4.POWER 5.DIVISION"))
+a=calculator(c)
+print(a)
+    
+
+
