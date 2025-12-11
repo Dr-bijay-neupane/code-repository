f731e36a179c99d9cc16c2b06be99702cd721146
Dr-bijay-neupane <bijayneupane433@gmail.com>
2025-12-11 21:40:54 +0545

    variable function

--- class.py
+++ class.py
@@ -1,7 +1,20 @@
 class computer:
-    def config
-
-
+    college="Gandaki college of engineering and science"
+    def __init__(self):
+        self.name="bijay"
+        self.age=21
 
 ob1=computer()
 ob2=computer()
+ob1.name="ramu"
+ob1.college="ACEM"
+print(ob1.name)
+print(ob1.age)
+print(ob1.college)
+print(ob2.name)
+print(ob2.age)
+print(ob2.college)
+    
+
+
+  
