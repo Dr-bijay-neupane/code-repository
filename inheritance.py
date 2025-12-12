c180fe1df5a7eded17523907afa852a83407c486
Dr-bijay-neupane <bijayneupane433@gmail.com>
2025-12-12 21:27:20 +0545

    INHERITANCE

---
+++ inheritance.py
@@ -0,0 +1,45 @@
+
+class A:
+
+    def __init__(self):
+        print("OPERATING from CLASS A")
+        
+
+    def feature(self):
+        print("FEATURE 1")
+
+
+    def feature2(self):
+        print("FEATURE 2")
+
+
+class B(A):
+
+    def __init__(self):
+        super( ).__init__()
+        print("OPERATING from CLASS B")
+
+    def feature3(self):
+        print("FEATURE 3")
+
+class C(B):
+
+    def __init__(self):
+        super().__init__()
+        print("OPERATING FROM C")
+
+    def feature4(self):
+        print("FEATURE 4")
+
+ob=A()
+ob.feature()
+ob.feature2()
+
+ob1=B()
+ob1.feature()
+ob1.feature2()
+
+ob1.feature3()
+
+ob2=C()
+ob2.feature4()
