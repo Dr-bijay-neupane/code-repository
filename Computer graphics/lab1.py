20c99bab17ed5baa587a7fd7d74ab7fb0c056d0b
Dr-bijay-neupane <bijayneupane433@gmail.com>
2025-12-25 19:26:05 +0545

    GRAPH

--- Computer graphics/lab1.py
+++ Computer graphics/lab1.py
@@ -1,6 +1,26 @@
-dx=1
-dy=3
+def DDA(x1,y1,x2,y2):
+    dx=x2-x1
+    dy=y2-y1
+    if abs(dx)>abs(dy):
+        steps=abs(dx)
+    else:
+        steps=abs(dy)
+    xinc=dx/steps
+    yinc=dy/steps
+    x=x1
+    y=y1
+    print(round(x), round(y))
+    for i in range (1,steps+1):
+        x=x+xinc
+        y=y+yinc
+        print(round(x), round(y))
+x1=int(input("ENTER X1"))
+y1=int(input("ENTER y1"))
+x2=int(input("ENTER X2"))
+y2=int(input("ENTER Y2"))
+DDA(x1,y1,x2,y2)
+    
 
-print(dx)
+        
 
-print(dy)
+
---
+++ Computer graphics/lab2.py
@@ -0,0 +1,45 @@
+import pygame
+import sys
+
+pygame.init()
+w,h=600,500
+screen = pygame.display.set_mode((w, h))
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def DDA(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+
+    if abs(dx) > abs(dy):
+        steps = abs(dx)
+    else:
+        steps = abs(dy)
+
+    xinc = dx / steps
+    yinc = dy / steps
+
+    x = x1
+    y = y1
+
+    for i in range(steps + 1):
+        
+        x = x + xinc
+        y = y + yinc
+        screen.set_at((round(x), round(y)), BLACK)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(WHITE)
+
+
+DDA(300, 150, 400, 250)
+
+    
